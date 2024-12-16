9e8429ab7ad6ba8b90100436fe81135ced690de5
Andrey-Zobnin <andy.zobnin@gmail.com>
2024-12-16 11:00:43 +0300

    update launguage, added comments to functions, fixed tabs

--- LabworkN3/Students.c
+++ LabworkN3/Students.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "Students.h"
 
+// функция идентификации студента для сввязи нескольких структур
 int IdentificationInCurrentStudent(Student* CurrentStudent) 
 {
     if (CurrentStudent == NULL) 
     {
         printf("Error: CurrentStudent is NULL.\n");
+        printf("Ошибка: CurrentStudent равен нулю.\n");
         return -1; 
     }
     else
@@ -14,11 +16,13 @@ int IdentificationInCurrentStudent(Student* CurrentStudent)
     }
 }
 
+// проверка на дурака в функции StudentsDataCheck 
 int StudentsDataCheck(Student* CurrentStudent) 
 {
     if (CurrentStudent == NULL) 
     {
         printf("CurrentStudent is NULL\n");
+        printf("Текущий студент не найден.\n");
         return 0; 
     } 
     else if (CurrentStudent->Age < 0 || CurrentStudent->Age > 100)
@@ -32,11 +36,12 @@ void DeltaAverageScore(Student* CurrentStudent, float NewScore)
 {
     if (StudentsDataCheck(CurrentStudent)) 
     {
-        CurrentStudent->AverageScore = NewScore;
+        CurrentStudent -> AverageScore = NewScore;
     }
     else
     {
         printf("invalid student data\n");
+        printf("невалидные данные студента\n");
     }
 }
 
@@ -44,12 +49,13 @@ int IsExcellentStudent(Student* CurrentStudent)
 {
     if (StudentsDataCheck(CurrentStudent)) 
     { 
-        return CurrentStudent->AverageScore >= 4.5; 
+        return CurrentStudent -> AverageScore >= 4.5; 
         // Отличник, если средний балл 4.5 или выше
     }
     else
     {
         printf("invalid of student data\n");
+        printf("невалидные данные студента в функции IsExcellentStudent\n");
         return 0;
     }   
 }
