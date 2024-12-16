6f43f9a9c168b9262d136f32baeaee48ec875ae4
Andrey-Zobnin <andy.zobnin@gmail.com>
2024-12-17 00:08:54 +0300

    added fuction check valid of name student, aded feature like idea to worked on, like to do

--- LabworkN3/Main.c
+++ LabworkN3/Main.c
@@ -8,6 +8,24 @@
 int GetStudentData(Student* CurrentStudent, int id);
 void PrintStudentData(Student* CurrentStudent);
 
+int IsValidName(const char* name) 
+{
+    if (name == NULL || strlen(name) == 0) 
+    {
+         // Имя пустое
+        printf("Name is empty\n");
+        printf("Имя пустое || равно нулю\n");
+        return 0;
+    }
+    else
+    {
+        printf("Имя принято || Имя корректное\n");
+        // Имя корректное
+    }
+    // при завершении логического блока мы пропускаем программу дальше и ничего не возврощаем кроме описания этого 
+    printf("Продолжайте ввод");
+
+}
 int GetStudentData(Student* CurrentStudent, int id) 
 {
     if (CurrentStudent == NULL) return 0;
@@ -17,12 +35,16 @@ int GetStudentData(Student* CurrentStudent, int id)
     printf("Введите имя студента: ");
     fgets(CurrentStudent -> Name, sizeof(CurrentStudent -> Name), stdin);
     CurrentStudent -> Name[strcspn(CurrentStudent->Name, "\n")] = '\0'; // Удаление символа новой строки
+    IsValidName(CurrentStudent -> Name); // Проверка имени
+    if (IsValidName(CurrentStudent -> Name) == 0) return 0;
+    // Проверяем корректность имени
 
     printf("Введите возраст студента: ");
+    
     char input[10];
     fgets(input, sizeof(input), stdin);
     
-    if (sscanf(input, "%d", &CurrentStudent->Age) != 1 || CurrentStudent->Age <= 0 || CurrentStudent->Age < 18) 
+    if (sscanf(input, "%d", &CurrentStudent -> Age) != 1 || CurrentStudent->Age <= 0 || CurrentStudent->Age < 18) 
     {
         printf("Ошибка: некорректный ввод возраста.\n");
         return 0; // Ошибка ввода
@@ -38,8 +60,10 @@ int GetStudentData(Student* CurrentStudent, int id)
     return 1; // Успех
 }
 
-void PrintStudentData(Student* CurrentStudent) {
-    if (CurrentStudent == NULL) {
+void PrintStudentData(Student* CurrentStudent) 
+{
+    if (CurrentStudent == NULL) 
+    {
         printf("Ошибка: студент не найден.\n");
         return;
     }
@@ -54,6 +78,35 @@ int main()
     Course curCourse = {"Computer Science - C", 0, 5};
     Student CurrentStudent;
     static int studentId = 1; // Статическая переменная для уникального ID студентов
+/*
+Idea to do in that project
+
+CurrentStudent.id = studentId;
+studentId++;
+    while (1)
+    {
+        printf("1. Добавить студента\n");
+        printf("2. Вывести данные студента\n");
+        printf("3. Выход\n");
+        int choice = 0;
+        scanf("%d", &choice);
+        switch (choice) 
+        {
+            case 1:
+                AddStudent(&CurrentStudent);
+                break;
+            case 2:
+                PrintStudentData(&CurrentStudent);
+                break;
+            case 3:
+                return 0;
+            default:
+                printf("Некорректный выбор. Повторите попытку.\n");
+            }
+    }
+
+
+*/    
 
     if (GetStudentData(&CurrentStudent, studentId++)) 
     {
