2a89978a60bb64548c7d930d6e238c981254ee48
Andrey-Zobnin <andy.zobnin@gmail.com>
2024-12-16 11:07:32 +0300

    update checkAge of student

--- LabworkN3/Main.c
+++ LabworkN3/Main.c
@@ -11,21 +11,21 @@ int GetStudentData(Student* CurrentStudent, int id)
 {
     if (CurrentStudent == NULL) return 0;
 
-    CurrentStudent->id = id; // Устанавливаем уникальный идентификатор
+    CurrentStudent- > id = id; // Устанавливаем уникальный идентификатор
 
     printf("Введите имя студента: ");
-    fgets(CurrentStudent->Name, sizeof(CurrentStudent->Name), stdin);
+    fgets(CurrentStudent->Name, sizeof(CurrentStudent -> Name), stdin);
     CurrentStudent->Name[strcspn(CurrentStudent->Name, "\n")] = '\0'; // Удаление символа новой строки
 
     printf("Введите возраст студента: ");
     char input[10];
     fgets(input, sizeof(input), stdin);
     
-    if (sscanf(input, "%d", &CurrentStudent->Age) != 1 || CurrentStudent->Age <= 0) {
+    if (sscanf(input, "%d", &CurrentStudent->Age) != 1 || CurrentStudent->Age <= 0 || CurrentStudent->Age < 18) 
+    {
         printf("Ошибка: некорректный ввод возраста.\n");
         return 0; // Ошибка ввода
     }
-
     printf("Введите средний балл студента: ");
     float avgGrade;
     if (scanf("%f", &avgGrade) != 1 || avgGrade < 0 || avgGrade > 5) {
